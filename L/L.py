ced52694da7d9d88792b228a3f30ee6d2086db15
Kibeom Kim <kkb110@kkim.(none)>
2010-12-01 15:19:49 -0500

    revised L

--- L/L.py
+++ L/L.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # fsmcompile -i ../letter_symbols.txt  -o ../word_symbols.txt -t transitions.txt  > L.fsm
+# fsmdraw -i ../letter_symbols.txt -o ../word_symbols.txt < L.fsm | dot -Tps > L.ps
 
 import re
 
 symbolList = []
 g = open("../word_symbols.txt")
+#g = open("labels.txt")
 
 while True:
 	tmp = g.readline().strip().split(' ')[0]
@@ -35,8 +37,8 @@ for symbol in symbolList:
 		else:
 			node.children[letter] = Node()
 		node = node.children[letter]
-	node.children["</w>"] = (stateIndex, symbol)
-	stateIndex = stateIndex + 1
+	node.children["</w>"] = (0, symbol)
+	#stateIndex = stateIndex + 1
 	 
 t = open("transitions.txt","w")
 
@@ -48,11 +50,12 @@ def x(node):
 	for letter,target in node.children.items():
 		if letter == "</w>":
 			print(node.index, node.children["</w>"][0], "</w>", node.children["</w>"][1], file=t)
-			print(node.children["</w>"][0], file=t)
+			#print(node.children["</w>"][0], file=t)
 		else:
 			print(node.index, node.children[letter].index, letter, "<eps>", file=t)
 			x(node.children[letter])
-
+			
 x(root)
-
+print(0, stateIndex, "</s>", "</s>", file=t)
+print(stateIndex, file=t)
 print("finished!")
