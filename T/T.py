4bcd189b575f7b0ca2d36467d9aee1be1e2d3e66
Kibeom Kim <kkb110@kkim.(none)>
2010-12-04 12:07:15 -0500

    nothing

--- T/T.py
+++ T/T.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 
 
-t = open("transitions.txt, w")
+import string
 
+
+t = open("transitions.txt", "w")
+
+
+# state state input output weight
+
+for input in string.ascii_lowercase:
+	for output in string.ascii_lowercase:
+		if input == output:
+			weight = 
+		else:
+			weight = 
+		print('0', '0', input, output, file=t)
+	
+for input in string.ascii_lowercase:
+	pass
+
+for output in string.ascii_lowercase:
+	pass
+
+
+print('0', file=t)
 
 
 
