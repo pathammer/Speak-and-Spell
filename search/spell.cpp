889b4ee7f223579c97896778f31b71455f8177e3
Patrick Hammer <pathammer@gmail.com>
2010-12-08 20:18:15 -0500

    Added Nathaniel's code.

--- search/spell.cpp
+++ search/spell.cpp
@@ -28,6 +28,39 @@ void demo() {
   printf("Reading fie file took %f seconds.\n", fileTimer.getClock());
 }
 
+int process() { //written by nathaiel
+	ofstream outFile;
+	int n = 10;
+	int m = 10;
+	int firstWord[m]; //so that it only composes secondWord the first time it's run
+	int secondWord[m]; //start symbol
+	int i = 0;
+	for (i = 0; i < m; i++) {
+		firstWord[i] = -1; //just a flag that it's the beginning of the file
+		secondWord[i] = 0; //NOTE: SHOULD BE START SYMBOL
+	}
+  string fileName;
+  fileName = "bestpathtest.txt";
+	int firstSymbol = 0;
+	int secondSymbol = 0;
+	//Begin Step 1
+	//Input inputWord ("input.txt");
+	//While there is still another word in the file: {
+	//string currWord = train2.getNext();
+	//End step 1
+	//Begin Step 2
+	//Run program to convert word to characer FST (through Kibeom's python program?)
+	//End step 2
+	//Begin step 3/4
+	//(through system?) something like: fstcompose char.fst TL.fst | fstshortestpath -numberPaths 10 | fstprint > "bestpathtest.txt"
+	//End step 3/4
+	//Begin Step 5
+	Input train (fileName);
+	outFile.open("newbestpathtest.txt");
+	return 0;
+}
+
+
 int main(int argc, char **argv) {
   string programName = "==========Speek-and-Spell!============\n";
   string authors = "Created by:\nNathaniel Weinman\tKibeom Kim\nPatrick Hammer\t\tAbhishek Suri\nShinan Zhang\t\tTaemin Cho\n";
