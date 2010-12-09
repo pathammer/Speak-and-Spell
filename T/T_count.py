d04c178ed8ddf9ccd34bbd4f72fc0e171002127f
Kibeom Kim <kkb110@gmail.com>
2010-12-09 07:23:07 -0500

    minor fix

--- T/T_count.py
+++ T/T_count.py
@@ -8,11 +8,19 @@
 import os
 import string
 from itertools import *
+import sys
+
+print("./T_count [T transducer file] [output count txt file]")
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
+
 
 #initialize count
 count = {}
 for input in chain(string.ascii_lowercase,['<eps>']):
 	for output in chain(string.ascii_lowercase,['<eps>']):
+		if input=='<eps>' and output=='<eps>':
+			continue
 		count[(input,output)] = 0
 
 
@@ -42,7 +50,7 @@ while True:
 	os.system("fstcompile --isymbols=../letter_symbols.txt --osymbols=../letter_symbols.txt tmp_WrongWord.txt > tmp_WrongWord.fst")
 	os.system("fstcompile --isymbols=../letter_symbols.txt --osymbols=../letter_symbols.txt tmp_CorrectWord.txt > tmp_CorrectWord.fst")
 	
-	os.system("fstcompose tmp_WrongWord.fst T.fst > tmp_Composed1.fst")
+	os.system("fstcompose tmp_WrongWord.fst " + filename1 + " > tmp_Composed1.fst")
 	os.system("fstcompose tmp_Composed1.fst tmp_CorrectWord.fst > tmp_Composed2.fst")
 	os.system("fstshortestpath tmp_Composed2.fst > tmp_ComposedShortestPath.fst")
 	os.system("fstprint --isymbols=../letter_symbols.txt --osymbols=../letter_symbols.txt tmp_ComposedShortestPath.fst > tmp_ComposedShortestPath.txt")
@@ -66,6 +74,6 @@ os.remove("tmp_Composed2.fst")
 os.remove("tmp_ComposedShortestPath.fst")
 os.remove("tmp_ComposedShortestPath.txt")
 
-CountFile = open("count.txt",'w')
+CountFile = open(filename2,'w')
 for key, value in count.items():
 	print(key[0], key[1], value,file=CountFile)
